8d733ddfff542a1ee960ac3a5ba5ef615bc69e21
Adriano Rocha <adriano.olr@gmail.com>
2021-08-25 21:42:48 -0300

    Middleware de autenticação

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.' })
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
