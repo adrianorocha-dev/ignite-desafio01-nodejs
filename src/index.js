0dfb761847cb5139ff88957199f8b71f0664452f
Adriano Rocha <adriano.olr@gmail.com>
2021-08-25 21:43:56 -0300

    Rota de completar todo

--- src/index.js
+++ src/index.js
@@ -85,7 +85,18 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'TODO não encontrado.' })
+  }
+
+  todo.done = true;
+
+  return response.status(200).json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
