b16ac6a21db1ce1aa9c303d2f95e70ea669b0699
Adriano Rocha <adriano.olr@gmail.com>
2021-08-25 21:43:41 -0300

    Rota de editar todo

--- src/index.js
+++ src/index.js
@@ -68,7 +68,20 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'TODO não encontrado.' })
+  }
+
+  todo.title = title ?? todo.title;
+  todo.deadline = deadline ?? todo.deadline;
+
+  return response.status(200).json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
